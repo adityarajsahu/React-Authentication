e32baeaf3417371854ffb32dbeda82c788edec19
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-06-19 17:11:09 +0530

    use context auth

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import Card from "../UI/Card/Card";
+import Button from "../UI/Button/Button";
 import classes from "./Home.module.css";
 
-const Home = () => {
+const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,7 +16,7 @@ const emailReducer = (state, action) => {
 
 const passwordReducer = (state, action) => {
   if (action.type === "USER_INPUT") {
-    return { value: action.val, isValid: action.trim().length > 6 };
+    return { value: action.val, isValid: action.val.trim().length > 6 };
   }
   if (action.type === "INPUT_BLUR") {
     return { value: state.value, isValid: state.value.trim().length > 6 };
